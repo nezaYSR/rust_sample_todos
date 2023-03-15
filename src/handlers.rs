c71cd657f519888040f4afc2f2b2909dbef0f29a
nezaYSR <jurukuncicpm@gmail.com>
2023-03-16 01:31:47 +0700

    initial push

---
+++ src/handlers.rs
@@ -0,0 +1,55 @@
+use crate::db;
+use crate::models::{CreateTodoList, Status};
+use actix_web::{web, HttpResponse, Responder};
+use deadpool_postgres::{Client, Pool};
+
+pub async fn status() -> impl Responder {
+    web::HttpResponse::Ok().json(Status {
+        status: "UP".to_string(),
+    })
+}
+
+pub async fn get_todos(db_pool: web::Data<Pool>) -> impl Responder {
+    let client: Client = db_pool
+        .get()
+        .await
+        .expect("Error connecting to the database");
+
+    let result = db::get_todos(&client).await;
+
+    match result {
+        Ok(todos) => HttpResponse::Ok().json(todos),
+        Err(_) => HttpResponse::InternalServerError().into(),
+    }
+}
+
+pub async fn get_items(db_pool: web::Data<Pool>, path: web::Path<(i32,)>) -> impl Responder {
+    let client: Client = db_pool
+        .get()
+        .await
+        .expect("Error connecting to the database");
+
+    let result = db::get_items(&client, path.0).await;
+
+    match result {
+        Ok(items) => HttpResponse::Ok().json(items),
+        Err(_) => HttpResponse::InternalServerError().into(),
+    }
+}
+
+pub async fn create_todo(
+    db_pool: web::Data<Pool>,
+    json: web::Json<CreateTodoList>,
+) -> impl Responder {
+    let client: Client = db_pool
+        .get()
+        .await
+        .expect("Error connecting to the database");
+
+    let result = db::create_todo(&client, json.title.clone()).await;
+
+    match result {
+        Ok(todo) => HttpResponse::Ok().json(todo),
+        Err(_) => HttpResponse::InternalServerError().into(),
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,37 @@
+mod config;
+mod db;
+mod handlers;
+mod models;
+
+use crate::handlers::*;
+use crate::models::Status;
+use actix_web::{web, App, HttpServer, Responder};
+use dotenv::dotenv;
+use std::io;
+use tokio_postgres::NoTls;
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    dotenv().ok();
+
+    let config = crate::config::Config::from_env().unwrap();
+
+    let pool = config.pg.create_pool(NoTls).unwrap();
+
+    println!(
+        "Starting server at http://{}:{}",
+        config.server.host, config.server.port
+    );
+
+    HttpServer::new(move || {
+        App::new()
+            .data(pool.clone())
+            .route("/", web::get().to(status))
+            .route("/todos{_:/?}", web::get().to(get_todos))
+            .route("/todos{_:/?}", web::post().to(create_todo))
+            .route("/items/{list_id}/items", web::get().to(get_items))
+    })
+    .bind(format!("{}:{}", config.server.host, config.server.port))?
+    .run()
+    .await
+}
